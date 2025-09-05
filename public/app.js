d44fe09dd0f665525ab4a286f5d80409d17dac94
byt3kno-source <byt3kno@gmail.com>
2025-09-05 11:35:20 +0300

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,45 +1,37 @@
 // Pavyzdinis duomenų rinkinys
 const products = {
   "Evaldas": [
-    { id: 1, name: "Fizzy Cherry Ice & Blueberry Cotton Candy", quantity: 10, sales: 0, profitCash: 0, profitBank: 0, takenForSelf: 0 },
-    { id: 2, name: "Mixed Berries & Double Apple Ice", quantity: 10, sales: 0, profitCash: 0, profitBank: 0, takenForSelf: 0 },
-    { id: 3, name: "Blue Razz Lemonade & Watermelon Bubblegum", quantity: 10, sales: 0, profitCash: 0, profitBank: 0, takenForSelf: 0 },
-    { id: 4, name: "Strawberry Watermelon Bubblegum & Mixed Fruit", quantity: 10, sales: 0, profitCash: 0, profitBank: 0, takenForSelf: 0 },
-    { id: 5, name: "Sour Apple Raspberry & Strawberry Big Bang", quantity: 10, sales: 0, profitCash: 0, profitBank: 0, takenForSelf: 0 },
-    { id: 6, name: "Peach Berry & Watermelon Mango Peach", quantity: 10, sales: 0, profitCash: 0, profitBank: 0, takenForSelf: 0 },
-    { id: 7, name: "Strawberry Cherry & Kiwi Passion Fruit", quantity: 10, sales: 0, profitCash: 0, profitBank: 0, takenForSelf: 0 },
-    { id: 8, name: "Gummy Bear & Strawberry Cola", quantity: 10, sales: 0, profitCash: 0, profitBank: 0, takenForSelf: 0 },
-    { id: 9, name: "Triple Melon Ice & Raspberry Watermelon", quantity: 10, sales: 0, profitCash: 0, profitBank: 0, takenForSelf: 0 }
+    { id: 1, name: "Fizzy Cherry Ice & Blueberry Cotton Candy", Kiekis: 10, Parduota: 0, Grynais: 0, Banku: 0, Sau: 0 },
+    { id: 2, name: "Mixed Berries & Double Apple Ice", Kiekis: 10, Parduota: 0, Grynais: 0, Banku: 0, Sau: 0 },
+    { id: 3, name: "Blue Razz Lemonade & Watermelon Bubblegum", Kiekis: 10, Parduota: 0, Grynais: 0, Banku: 0, Sau: 0 },
+    { id: 4, name: "Strawberry Watermelon Bubblegum & Mixed Fruit", Kiekis: 10, Parduota: 0, Grynais: 0, Banku: 0, Sau: 0 },
+    { id: 5, name: "Sour Apple Raspberry & Strawberry Big Bang", Kiekis: 10, Parduota: 0, Grynais: 0, Banku: 0, Sau: 0 },
+    { id: 6, name: "Peach Berry & Watermelon Mango Peach", Kiekis: 10, Parduota: 0, Grynais: 0, Banku: 0, Sau: 0 },
+    { id: 7, name: "Strawberry Cherry & Kiwi Passion Fruit", Kiekis: 10, Parduota: 0, Grynais: 0, Banku: 0, Sau: 0 },
+    { id: 8, name: "Gummy Bear & Strawberry Cola", Kiekis: 10, Parduota: 0, Grynais: 0, Banku: 0, Sau: 0 },
+    { id: 9, name: "Triple Melon Ice & Raspberry Watermelon", Kiekis: 10, Parduota: 0, Grynais: 0, Banku: 0, Sau: 0 }
   ],
   "Dovydas": [
-    { id: 1, name: "Watermelon Ice & Lemon Lime", quantity: 10, sales: 0, profitCash: 0, profitBank: 0, takenForSelf: 0 },
-    { id: 2, name: "Grape Ice & Strawberry Kiwi", quantity: 10, sales: 0, profitCash: 0, profitBank: 0, takenForSelf: 0 },
-    { id: 3, name: "Strawberry Raspberry Cherry & Love 666", quantity: 20, sales: 0, profitCash: 0, profitBank: 0, takenForSelf: 0 },
-    { id: 4, name: "Cherry Cola Ice & Strawberry Raspberry Candy", quantity: 10, sales: 0, profitCash: 0, profitBank: 0, takenForSelf: 0 },
-    { id: 5, name: "Blueberry Ice & Black Dragon Ice", quantity: 10, sales: 0, profitCash: 0, profitBank: 0, takenForSelf: 0 },
-    { id: 6, name: "Strawberry Cherry & Kiwi Passion Fruit", quantity: 10, sales: 0, profitCash: 0, profitBank: 0, takenForSelf: 0 },
-    { id: 7, name: "Banana Pineapple Ice & Red Bull Ice", quantity: 10, sales: 0, profitCash: 0, profitBank: 0, takenForSelf: 0 },
-    { id: 8, name: "Red Bull Strawberry & Blackcurrant Ice", quantity: 10, sales: 0, profitCash: 0, profitBank: 0, takenForSelf: 0 }
+    { id: 1, name: "Watermelon Ice & Lemon Lime", Kiekis: 10, Parduota: 0, Grynais: 0, Banku: 0, Sau: 0 },
+    { id: 2, name: "Grape Ice & Strawberry Kiwi", Kiekis: 10, Parduota: 0, Grynais: 0, Banku: 0, Sau: 0 },
+    { id: 3, name: "Strawberry Raspberry Cherry & Love 666", Kiekis: 20, Parduota: 0, Grynais: 0, Banku: 0, Sau: 0 },
+    { id: 4, name: "Cherry Cola Ice & Strawberry Raspberry Candy", Kiekis: 10, Parduota: 0, Grynais: 0, Banku: 0, Sau: 0 },
+    { id: 5, name: "Blueberry Ice & Black Dragon Ice", Kiekis: 10, Parduota: 0, Grynais: 0, Banku: 0, Sau: 0 },
+    { id: 6, name: "Strawberry Cherry & Kiwi Passion Fruit", Kiekis: 10, Parduota: 0, Grynais: 0, Banku: 0, Sau: 0 },
+    { id: 7, name: "Banana Pineapple Ice & Red Bull Ice", Kiekis: 10, Parduota: 0, Grynais: 0, Banku: 0, Sau: 0 },
+    { id: 8, name: "Red Bull Strawberry & Blackcurrant Ice", Kiekis: 10, Parduota: 0, Grynais: 0, Banku: 0, Sau: 0 }
   ]
 };
 
 // Prisijungimo pasirinkimas
-let currentUser = null;
-let isAdmin = false;
+let isAdmin = true;
 
 function login() {
   const userChoice = prompt("Choose user: Evaldas, Dovydas or Admin");
 
-  if (userChoice === "Evaldas") {
-    currentUser = "Evaldas";
-  } else if (userChoice === "Dovydas") {
-    currentUser = "Dovydas";
-  } else if (userChoice === "Admin") {
+  if (userChoice === "Admin") {
     currentUser = "Admin";
     isAdmin = true;
-  } else {
-    alert("Invalid user!");
-    return;
   }
 
   loadProducts();
